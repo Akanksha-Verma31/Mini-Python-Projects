1d821c64dcd623e1f04859ca94b97448c5092a91
Akanksha Verma <22906.akanksha@gmail.com>
2021-08-30 13:30:40 +0530

    Update Internet Speed Checker.py

--- Internet Speed Checker.py
+++ Internet Speed Checker.py
@@ -14,7 +14,7 @@ root.config(bg="pink")   # to set the bg color
 root.geometery("500x250")
 
 label1 = Label(root,text="Internet Speed Checker",font=("arial",30,"bold"),bg="lightblue").pack()    # pack() will allow to put these line by line
-button1 = Button(root,text="Click !!",font=("arial",20,"bold"),bg="yellow,height=1,width=10,command=one).pack()
+button1 = Button(root,text="Click !!",font=("arial",20,"bold"),bg="yellow",height=1,width=10,command=one).pack()
                  
                  
 root.mainloop()
